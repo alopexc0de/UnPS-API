7b4d4da8f64cab73f75654deb03b452491d4f2f4
Arctic Code <tehfoxy.c0de@gmail.com>
2013-07-26 22:44:05 -0500

    Test of regUser function - Test sucessful
    upImage needs to be tested, but I'm lazy
    regAPI to be tested next

--- api.test.php
+++ api.test.php
@@ -10,6 +10,8 @@ $unpsAPI = new api();
 //echo $unpsAPI->shorten($apidb, '580658027', $shortdb, '[Full URL]');
 //echo $unpsAPI->delShort($apidb, '580658027', $shortdb, '[Short link Code Only]]');
 
-echo $unpsAPI->upImage($apidb, '580658027', $imgdb, 'dc0de', 'This is a test', 'test', 0, imgdata)
+//echo $unpsAPI->upImage($apidb, '580658027', $imgdb, 'dc0de', 'This is a test', 'test', 0, imgdata)
+
+echo $unpsAPI->regUser($apidb, '580658027', $udb, 'David', 'password123', 'tehfoxy.c0de@gmail.com');
 
 ?>
