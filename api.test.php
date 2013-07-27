051c7f060f446c76e2151647eeee0ca8bf655f0c
Arctic Code <tehfoxy.c0de@gmail.com>
2013-07-26 23:35:47 -0500

    Test reset API key - Successful

--- api.test.php
+++ api.test.php
@@ -13,6 +13,8 @@ $unpsAPI = new api();
 //echo $unpsAPI->upImage($apidb, '580658027', $imgdb, 'dc0de', 'This is a test', 'test', 0, imgdata); // Upload image 
 
 //echo $unpsAPI->regUser($apidb, '580658027', $udb, 'David', 'password123', 'tehfoxy.c0de@gmail.com'); // Register users
-echo $unpsAPI->regAPI($apidb, '580658027', 'UnPS-GAMA Link Shortener', 'tehfoxy.c0de@gmail.com', '1,0,0,0'); // Register API user
+//echo $unpsAPI->regAPI($apidb, '580658027', 'UnPS-GAMA Link Shortener', 'tehfoxy.c0de@gmail.com', '1,0,0,0'); // Register API user
+
+echo $unpsAPI->resetAPI($apidb, '580658027', 'UnPS-GAMA Link Shortener', 'tehfoxy.c0de@gmail.com', '279qit9');
 
 ?>
